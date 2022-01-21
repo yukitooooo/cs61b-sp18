dc12fe8cfe4fc1ee367613bfd2e4d1b8aeb7b0b6
Romain <romain@192.168.0.101>
2022-01-22 03:33:49 +0800

    使用>>>与size最近的2^N

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -21,7 +21,8 @@ public class ArrayDeque<T> {
         items[nextFirst] = item;
         size++;
         if (nextFirst == nextLast) {
-            reSize(size * 2);
+            int p = calculatesize(size);
+            reSize(p);
         }
     }
 
@@ -33,7 +34,8 @@ public class ArrayDeque<T> {
         nextLast = (nextLast + 1) & (items.length - 1);
         size++;
         if (nextLast == nextFirst) {
-            reSize(size * 2);
+            int p = calculatesize(size);
+            reSize(p);
         }
 
         //在双端队列的后面添加一个类型的项目
@@ -52,13 +54,26 @@ public class ArrayDeque<T> {
         nextLast = n;
     }
 
+    private static int calculatesize(int num) {
+        int initialCapacity = 8;
+        if (num >= initialCapacity) {
+            initialCapacity = num;
+            initialCapacity |= (initialCapacity >>> 1);
+            initialCapacity |= (initialCapacity >>> 2);
+            initialCapacity |= (initialCapacity >>> 4);
+            initialCapacity |= (initialCapacity >>> 8);
+            initialCapacity |= (initialCapacity >>> 16);
+            initialCapacity++;
+            if (initialCapacity < 0) {
+                initialCapacity >>>= 1;
+            }
+        }
+        return initialCapacity;
+    }
+
     public boolean isEmpty() {
         return size == 0;
         //如果 deque 为空，则返回 true，否则返回 false
-    }
-
-    private boolean isLarage() {
-        return items.length >= 16 && size() / (double) items.length < 0.25;
     }
 
     public int size() {
@@ -87,10 +102,9 @@ public class ArrayDeque<T> {
         if (t == null) {
             return null;
         }
-        if (isLarage()) {
-            reSize(size / 2);
-        }
         size--;
+        int p = calculatesize(size);
+        reSize(p);
         return t;
 
         //删除并返回双端队列前面的项目。如果不存在这样的项目，则返回 null。
@@ -115,17 +129,16 @@ public class ArrayDeque<T> {
         if (t == null) {
             return null;
         } else {
-            if (isLarage()) {
-                reSize(size / 2);
-            }
             size--;
+            int p = calculatesize(size);
+            reSize(p);
             return t;
         }
 
         //删除并返回双端队列后面的项目。如果不存在这样的项目，则返回 null。
     }
 
-    private  T findLast() {
+    private T findLast() {
         int p = (nextLast - 1) & (items.length - 1);
         T t = items[p];
         if (t == null) {
@@ -155,5 +168,4 @@ public class ArrayDeque<T> {
         // 其中 0 是前面，1 是下一个项目，依此类推。
         // 如果不存在这样的项目，则返回 null。不能改变双端队列！
     }
-
 }
