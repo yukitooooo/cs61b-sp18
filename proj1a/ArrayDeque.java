c667f48051590431cf549a76dfeb52e9a53202ea
Romain <skymed@192.168.0.100>
2022-01-30 18:00:39 +0800

    lib

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -199,36 +199,23 @@ public class ArrayDeque<T> {
         return size() > 8 && (size() / (double) items.length) < 0.25;
 
     }
-//    public static void main(String[] args) {
-//        ArrayDeque<Integer> p = new ArrayDeque<>();
-//        for (int i = 0; i < 2048; i++) {
-//            p.addFirst(i);
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.removeFirst();
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.addLast(i);
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.removeLast();
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.addLast(i);
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.removeLast();
-//
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.addFirst(i);
-//        }
-//        for (int i = 0; i < 2048; i++) {
-//            p.removeFirst();
-//        }
-//        System.out.println(p.removeLast());
-//        p.printDeque();
-//
-//
-//    }
+    public static void main(String[] args) {
+        ArrayDeque<Integer> p = new ArrayDeque<>();
+        for (int i = 0; i < 33; i++) {
+            p.addFirst(i);
+        }
+        for (int i = 0; i < 33; i++) {
+            p.removeFirst();
+        }
+        for (int i = 0; i < 33; i++) {
+            p.addLast(i);
+        }
+        for (int i = 0; i < 2048; i++) {
+            p.removeLast();
+        }
+        System.out.println(p.removeLast());
+        p.printDeque();
+
+
+    }
 }
